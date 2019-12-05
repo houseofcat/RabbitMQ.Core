f77399fd138688287724e43edc51571c2e2253e9
Tristan Hyams <cat@houseofcat.io>
2019-12-05 13:22:12 -0500

    Configuration cleanup.

--- v5.1.2/RabbitMQ.Client/Properties/AssemblyInfo.cs
+++ v5.1.2/RabbitMQ.Client/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("RabbitMQ Client Library for .NET")]
-[assembly: AssemblyDescription("See http://rabbitmq.com")]
+[assembly: AssemblyDescription("View https://github.com/houseofcat/RabbitMQ.Core for more details.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Pivotal Software, Inc.")]
-[assembly: AssemblyProduct("RabbitMQ")]
+[assembly: AssemblyCompany("HouseofCat.io")]
+[assembly: AssemblyProduct("RabbitMQ.Core.Client")]
 [assembly: AssemblyCopyright("Copyright © 2007-2016 Pivotal Software, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("4.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
