f997320f65d71a9202e2e30fb3b684d4d2255ece
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2020-07-28 21:27:15 -0400

    Update ConnectionHost.cs

--- CookedRabbit.Core/Pools/ConnectionHost.cs
+++ CookedRabbit.Core/Pools/ConnectionHost.cs
@@ -21,7 +21,7 @@ namespace CookedRabbit.Core.Pools
         Task<bool> HealthyAsync();
     }
 
-    public class ConnectionHost : IConnectionHost
+    public class ConnectionHost : IConnectionHost, IDisposable
     {
         public IConnection Connection { get; private set; }
         public ulong ConnectionId { get; }
@@ -31,7 +31,8 @@ namespace CookedRabbit.Core.Pools
         public bool Closed { get; private set; }
 
         private readonly ILogger<ConnectionHost> _logger;
-        private readonly SemaphoreSlim hostLock = new SemaphoreSlim(1, 1);
+        private readonly SemaphoreSlim _hostLock = new SemaphoreSlim(1, 1);
+        private bool _disposedValue;
 
         public ConnectionHost(ulong connectionId, IConnection connection)
         {
@@ -43,7 +44,7 @@ namespace CookedRabbit.Core.Pools
 
         public void AssignConnection(IConnection connection)
         {
-            hostLock.Wait();
+            _hostLock.Wait();
 
             if (Connection != null)
             {
@@ -64,31 +65,31 @@ namespace CookedRabbit.Core.Pools
             Connection.ConnectionUnblocked += ConnectionUnblocked;
             Connection.ConnectionShutdown += ConnectionClosed;
 
-            hostLock.Release();
+            _hostLock.Release();
         }
 
         private void ConnectionClosed(object sender, ShutdownEventArgs e)
         {
-            hostLock.Wait();
+            _hostLock.Wait();
             _logger.LogWarning(e.ReplyText);
             Closed = true;
-            hostLock.Release();
+            _hostLock.Release();
         }
 
         private void ConnectionBlocked(object sender, ConnectionBlockedEventArgs e)
         {
-            hostLock.Wait();
+            _hostLock.Wait();
             _logger.LogWarning(e.Reason);
             Blocked = true;
-            hostLock.Release();
+            _hostLock.Release();
         }
 
         private void ConnectionUnblocked(object sender, EventArgs e)
         {
-            hostLock.Wait();
+            _hostLock.Wait();
             _logger.LogInformation("Connection unblocked!");
             Blocked = false;
-            hostLock.Release();
+            _hostLock.Release();
         }
 
         private const int CloseCode = 200;
@@ -104,7 +105,7 @@ namespace CookedRabbit.Core.Pools
         /// </summary>
         public async Task<bool> HealthyAsync()
         {
-            await hostLock
+            await _hostLock
                 .WaitAsync()
                 .ConfigureAwait(false);
 
@@ -113,9 +114,30 @@ namespace CookedRabbit.Core.Pools
             else if (Dead && Connection.IsOpen)
             { Dead = false; } // Means a Miracle took place.
 
-            hostLock.Release();
+            _hostLock.Release();
 
             return Connection.IsOpen && !Blocked; // TODO: See if we can incorporate Dead/Closed observations.
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _hostLock.Dispose();
+                }
+
+                Connection = null;
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
